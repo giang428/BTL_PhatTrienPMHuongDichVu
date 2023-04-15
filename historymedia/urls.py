fe65fa96e4391f0a7c32106775f25d4caf99670f
nguyendinhnguyen2001 <nguyendinhnguyen2903@gmail.com>
2023-04-15 23:38:14 +0700

    update api get_history_media

--- historymedia/urls.py
+++ historymedia/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from historymedia import views
 
 urlpatterns = [
-    path("createhistorymedia",views.post_history_media,name="createhistorymedia"),
-    path("getlisthistorybyuserid/<int:id_user>",views.get_list_history_media_by_userid,name="getlisthistorybyuserid"),
-    path("updatehistorymedia",views.update_history_media,name="updatehistorymedia"),
+    path("createhistorymedia", views.post_history_media, name="createhistorymedia"),
+    path("getlisthistorybyuserid/<int:id_user>",
+         views.get_list_history_media_by_userid, name="getlisthistorybyuserid"),
+    path("updatehistorymedia", views.update_history_media,
+         name="updatehistorymedia"),
+    path("gethistorymedia", views.get_history_media, name="gethistorymedia"),
 ]
